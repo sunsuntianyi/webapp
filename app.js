f9d2a08475586ad2c511ad629362ca598c1ce247
sunsuntianyi <38114807+sunsuntianyi@users.noreply.github.com>
2018-10-08 03:02:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ var bodyParser = require('body-parser');
 /* connect to database */
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/inception');
+mongoose.connect('mongodb://localhost:27017/inception');
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to database");
+});
+mongoose.connection.on("error", (err) => {
+	console.log("Database error:" + err);
+});
+
+
 require("./models/Photo");
 
 /* Define routes */
